69bb39af9ac52bf1f81a8fda295f5432ccf82754
Thomas Dawson <dawsontc003@gmail.com>
2020-12-10 21:57:04 -0700

    issue with save button

--- Develop/server.js
+++ Develop/server.js
@@ -2,10 +2,8 @@
 const express = require("express");
 // requiring "path" shipped module
 const path = require("path");
-// requiring "console" shipped module
-const { table, dir } = require("console");
-const { dirname } = require("path");
-const { Dir } = require("fs");
+
+const fs = require("fs");
 // Declaring variable app to store express function output
 const app = express();
 // declaring variable PORT as desired port number for server
@@ -14,21 +12,26 @@ const PORT = 9000;
 // Middleware express code
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 // inherited request response function to send index html file with desired path
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 // inherited request response function to send notes html file with desired path
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+// route for get notes
+app.get("/api/notes", (req, res) => {
+  console.log("get api working");
+  res.end();
 });
 
 // Create New Characters - takes in JSON input
 app.post("/api/notes", (req, res) => {
   const testPost = req.body;
 
-  console.log(testPost);
+  console.log("testPost");
   res.end();
 });
 

d2b16b3fcfc5b7fa906c114fa3420d8d99fe4cec
Thomas Dawson <dawsontc003@gmail.com>
2020-12-14 00:27:47 -0700

    Final push with working note get and post APIs

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -87,7 +87,7 @@ const handleNoteDelete = (e) => {
   if (activeNote.id === noteId) {
     activeNote = {};
   }
-
+  console.log("noteId:", noteId);
   deleteNote(noteId).then(() => {
     getAndRenderNotes();
     renderActiveNote();
--- Develop/server.js
+++ Develop/server.js
@@ -3,8 +3,11 @@ const express = require("express");
 // requiring "path" shipped module
 const path = require("path");
 
+const { v4: uuidv4 } = require("uuid");
+
 const fs = require("fs");
 const { stringify } = require("querystring");
+const { response } = require("express");
 // Declaring variable app to store express function output
 const app = express();
 // declaring variable PORT as desired port number for server
@@ -14,6 +17,7 @@ const PORT = 9000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+let notes = [];
 // inherited request response function to send index html file with desired path
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -26,30 +30,54 @@ app.get("/notes", (req, res) => {
 app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
-    //console.log(JSON.parse(data));
-    const note = JSON.parse(data);
-    console.log(note);
+    console.log("json.parse: ", data);
+    notes = JSON.parse(data);
+    console.log(notes);
+    res.send(notes);
   });
   console.log("API to get notes");
 
-  res.end();
+  // res.end();
 });
-// array for notes
-const notes = [];
 
 // Create New Characters - takes in JSON input
 app.post("/api/notes", (req, res) => {
-  let note = JSON.stringify(req.body);
+  console.log("req.body: ", req.body);
+  let note = req.body;
+  note.id = uuidv4();
   notes.push(note);
 
   fs.writeFile(
     "./db/db.json",
-    `[${notes}]`,
+    `${JSON.stringify(notes)}`,
     // error handling and success message to advise commandline user of error or succes
     (err) => (err ? console.error(err) : console.log("Success!"))
   );
   //console.log(notes);
   res.end();
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  console.log("req.params.id: ", req.params.id);
+  let noteID = req.params.id;
+
+  //noteIndex = notes.findIndex((note) => note.id === noteID);
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id === noteID) {
+      notes.slice(i, 1);
+      console.log("i", i);
+      console.log("notes: ", notes);
+      break;
+    }
+  }
+
+  fs.writeFile(
+    "./db/db.json",
+    `${JSON.stringify(notes)}`,
+    // error handling and success message to advise commandline user of error or succes
+    (err) => (err ? console.error(err) : res.send(notes))
+  );
+  //console.log(notes);
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
